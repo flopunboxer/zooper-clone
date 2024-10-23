1325f14348866a65207a9a8a52d7b2f7093c2411
flopunboxer <flopunboxer@gmail.com>
2024-10-23 17:04:38 +0530

    apis updated new

--- src/app/api/submitForm/route.js
+++ src/app/api/submitForm/route.js
@@ -5,14 +5,6 @@ import User from '@/utils/mongo/models/userSchema'
 import nodemailer from 'nodemailer';
 
 export async function POST(req) {
-  // Allow CORS
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
   console.log("trying db...")
   await dbConnect();
   
