73236629bcd783522461a243d150a1ac1c2280f4
flopunboxer <flopunboxer@gmail.com>
2024-10-23 16:42:26 +0530

    apis updated

--- src/app/api/checkUser/route.js
+++ src/app/api/checkUser/route.js
@@ -11,17 +11,17 @@ export async function POST(req) {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
+
+  // await dbConnect();
   
-  await dbConnect();
-  
-  const { email } = await req.json();
+  // const { email } = await req.json();
 
   try {
-    const user = await User.findOne({ email });
-    if (user) {
+    // const user = await User.findOne({ email });
+    // if (user) {
       return new Response(JSON.stringify({ exists: true,code:200,data:user }), { status: 200 });
-    }
-    return new Response(JSON.stringify({ exists: false,code:200 }), { status: 200 });
+    // }
+    // return new Response(JSON.stringify({ exists: false,code:200 }), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ message: 'Server Error',code:500 }), { status: 500 });
   }
