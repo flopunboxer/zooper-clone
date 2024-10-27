2fafdf8d5445799ac2a62cccc3248188246dbd07
flopunboxer <flopunboxer@gmail.com>
2024-10-27 13:09:26 +0530

    changes done in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        MONGODB_URI: "mongodb+srv://robin:d0Txpxjb86DO7P2b@zooper-clone.6ffrv.mongodb.net/",
-        EMAIL_USER:"robindeep.eminence@gmail.com",
-        EMAIL_PASS:"veod zcpx yehx wmww",
-        ADMIN_EMAIL:"robindeep.eminence@gmail.com"
+        MONGODB_URI: "mongodb+srv://infobookingsyoohooindia:lkjpqpDGQNdAPL2p@yoohoo-india.fkwea.mongodb.net/Yoohoo-Database",
+        EMAIL_USER:"infobookings.yoohooindia@gmail.com",
+        EMAIL_PASS:"humw fadz yukr owuw",
+        ADMIN_EMAIL:"infobookings.yoohooindia@gmail.com"
       },
       
 };
